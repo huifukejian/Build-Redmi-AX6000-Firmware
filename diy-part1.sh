d3a13fac399305744810de07db15f6837a7b63e0
huifukejian <xmw486@gmail.com>
2025-09-10 12:01:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,6 +3,7 @@
 git clone --filter=blob:none --depth 1 --single-branch https://github.com/pymumu/openwrt-smartdns package/custom/smartdns
 git clone --filter=blob:none --depth 1 --single-branch https://github.com/pymumu/luci-app-smartdns -b master package/custom/luci-app-smartdns
 git clone --filter=blob:none --depth 1 --single-branch https://github.com/xiaorouji/openwrt-passwall -b main package/custom/luci-app-passwall
+git clone --filter=blob:none --depth 1 --single-branch https://github.com/xiaorouji/openwrt-passwall2 -b main package/custom/luci-app-passwall2
 git clone --filter=blob:none --depth 1 --single-branch https://github.com/xiaorouji/openwrt-passwall-packages -b main package/custom/passwall-packages
 git clone --filter=blob:none --depth 1 --single-branch https://github.com/tty228/luci-app-wechatpush -b master package/custom/luci-app-wechatpush
 git clone --filter=blob:none --depth 1 --single-branch https://github.com/hubbylei/wrtbwmon -b master package/custom/wrtbwmon
